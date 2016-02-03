c194370b1bcc49b5220518af7698b63c34826221
Matt Johnson <johnsonma@Matt-Johnsons-MacBook-Pro-2.local>
2016-02-02 22:56:52 -0800

    More bug fixes, experimental effects.

--- luxe_ascii/TextBuffer.hx
+++ luxe_ascii/TextBuffer.hx
@@ -2,6 +2,7 @@ package luxe_ascii;
 
 import luxe.Color;
 import snow.api.buffers.*;
+import luxe.Rectangle;
 
 class TextBuffer {
 	
@@ -42,7 +43,10 @@ class TextBuffer {
 		if(x < 0 || y < 0 || x >= width || y >= height)
 			return;
 		
-		data[y * width + x].fg = color;
+		var c:Color = data[y * width + x].fg;
+		c.r = color.r;
+		c.g = color.g;
+		c.b = color.b;
 	}
 
 	public function set_bg_color(x:Int, y:Int, color:Color) {
@@ -80,13 +84,67 @@ class TextBuffer {
 				if(src_data.attr & 0x02 != 0)
 					continue;
 
-				dst_data.fg = src_data.fg;
+				dst_data.fg.r = src_data.fg.r;
+				dst_data.fg.g = src_data.fg.g;
+				dst_data.fg.b = src_data.fg.b;
+
 				dst_data.char = src_data.char;
 				dst_data.bg[0] = src_data.bg[0];
 				dst_data.bg[1] = src_data.bg[1];
 				dst_data.bg[2] = src_data.bg[2];
 				dst_data.attr = src_data.attr;
 			}
+		}
+	}
+
+	public function darken(_x:Int, _y:Int, w:Int, h:Int) {
+
+		for(y in _y ... _y + h) {
+			for(x in _x ... _x + w) {
+				var d = data[y * width + x];
+
+				d.fg.r *= 0.5;
+				d.fg.g *= 0.5;
+				d.fg.b *= 0.5;
+
+				d.bg[0] >>= 1;
+				d.bg[1] >>= 1;
+				d.bg[2] >>= 1;		
+			}
+		}
+	}
+
+	public function grayscale(_x:Int, _y:Int, w:Int, h:Int, ?amount:Float = 1.0) {
+
+		for(y in _y ... _y + h) {
+			for(x in _x ... _x + w) {
+				var d = data[y * width + x];
+
+				var f:Float = d.fg.r * 0.3 + d.fg.g * 0.6 + d.fg.b * 0.11;
+
+				d.fg.r = amount * f + (1 - amount) * d.fg.r;
+				d.fg.g = amount * f + (1 - amount) * d.fg.g;
+				d.fg.b = amount * f + (1 - amount) * d.fg.b;
+
+				d.bg[0] >>= 1;
+				d.bg[1] >>= 1;
+				d.bg[2] >>= 1;		
+			}
+		}
+	}
+
+	public function clear(c:Color) {
+		for(i in 0 ... width * height) {
+
+			var d:CharAttr = data[i];
+			d.fg.r = 0;
+			d.fg.g = 0;
+			d.fg.b = 0;
+			// d.fg.r = d.fg.g = d.fg.b = 0; // this broke it...
+			d.char = 0;
+			d.bg[0] = Std.int(c.r * 255);
+			d.bg[1] = Std.int(c.g * 255);
+			d.bg[2] = Std.int(c.b * 255);
 		}
 	}
 }
